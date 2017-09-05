7e4b3aa1b020f9a57b464a8021aaec8b7df8dd4f
路遥 <liluyao.vip@gmail.com>
2017-09-05 09:38:01 +0800

    express-note

---
+++ express-note/src/js/mod/toast.js
@@ -0,0 +1,12 @@
+function Toast(msg, time) {
+    this.msg = msg;
+    this.time = time || 1000;
+    this.createToast();
+    this.showToast();
+}
+
+Toast.prototype = {
+    createToast: function() {
+        var 
+    }
+}
