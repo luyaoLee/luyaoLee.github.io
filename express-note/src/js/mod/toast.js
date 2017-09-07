72601268ca90bfcb3679b602e96caee4fc402094
路遥 <liluyao.vip@gmail.com>
2017-09-07 14:19:29 +0800

    express-note

--- express-note/src/js/mod/toast.js
+++ express-note/src/js/mod/toast.js
@@ -1,12 +1,31 @@
-function Toast(msg, time) {
+var less = require('less/toast.less');
+var $ = require('jquery');
+
+function _Toast(msg, time) {
     this.msg = msg;
-    this.time = time || 1000;
+    this.dismissTime = time || 1000;
     this.createToast();
     this.showToast();
 }
 
-Toast.prototype = {
+_Toast.prototype = {
     createToast: function() {
-        var 
+        var tpl = '<div class="toast" style="display: none;">'+ this.msg + '</div>';
+        this.$toast = $(tpl);
+        $('body').append(this.$toast);
+    },
+    showToast: function() {
+        var _this = this;
+        this.$toast.fadeIn(function() {
+            setTimeout(function() {
+                _this.$toast.fadeOut();
+            }, _this.dismissTime);
+        });
     }
 }
+
+function Toast(msg, time) {
+    return new _Toast(msg, time);
+}
+
+module.exports.Toast = Toast;
--- express-note/src/webpack.config.js
+++ express-note/src/webpack.config.js
@@ -6,13 +6,22 @@ module.exports = {
     output: {
         path: path.join(__dirname, "../public/js"),
         filename: "index.js"
+    },
+    //路径解析
+    resolve: {
+        alias: {
+            jquery: path.join(__dirname, "./js/lib/jquery-2.1.4.min.js"),
+            mod: path.join(__dirname, "./js/mod"),
+            less: path.join(__dirname, "./less")
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                //use中的loader执行顺序是从右向左
+                use: ["style-loader", "css-loader", "less-loader"]
+            }
+        ]
     }
-    // module: {
-    //     rules: [
-    //         {
-    //             test: /\.less$/,
-    //             use: ["style-loader", "css-loader", "less-loader"]
-    //         }
-    //     ]
-    // }
 }
