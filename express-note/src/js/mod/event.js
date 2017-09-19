112afd351c90188fe4372a3fc40e42e83c5ee195
路遥 <luyao_lee@163.com>
2017-09-19 22:23:21 +0800

    es6

--- express-note/src/js/mod/event.js
+++ express-note/src/js/mod/event.js
@@ -1,33 +1,30 @@
-var EventCenter = (function() {
-
-    var events = {};
-
-    function on(evt, handler) {
+let EventCenter = (() => {
+    let events = {};
+    let on = (evt, handler) => {
         events[evt] = events[evt] || [];
-
-        events[evt].push({handler: handler});
+        events[evt].push({
+            handler: handler
+        });
     }
-
-    function fire(evt, args) {
-        if (!events[evt]) {
-            return;
+    let off = evt => {
+        if(events[evt]) {
+            delete events[evt];
         }
-        for (var i = 0; i < events[evt].length; i++) {
+    }
+    let fire = (evt, args) => {
+        if (!events[evt]) return;
+
+        for (let i = 0; i < events[evt].length; i++) {
             events[evt][i].handler(args);
         }
     }
 
-    return {on: on, fire: fire}
+    return {
+        on: on,
+        off: off,
+        fire: fire
+    }
 })();
 
+window.EventCenter = EventCenter;
 module.exports = EventCenter;
-
-// EventCenter.on('text-change', function(data){
-//  console.log(data);
-// });
-
-// EventCenter.on('text-change', function(data){
-//  alert(1);
-// });
-
-// EventCenter.fire('text-change', 100);
